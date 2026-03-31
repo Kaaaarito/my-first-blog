cd255e9a6ace33df7e83bcd96b051b7b96e46b39
Karol <karol.ba0911@gmail.com>
2026-03-31 11:30:32 -0500

    otras mejoras

--- mysite/settings.py
+++ mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j8*h#f92jf092jf029jf029f2!#%$&hjklsdfghjkl'
+SECRET_KEY = 'j8*h#f92jdqwjfowqiropwiqieopqjwroihsflahfjashfoqwfoqfhoqhwf92jf029jf029f2!#%$&hjklsdfghjkl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -129,3 +129,6 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 
 SECURE_HSTS_SECONDS = 31536000
+
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
